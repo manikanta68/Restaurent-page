264d9db85152e78e806548cdc66d23360bbd97e5
manikant68 <smanikanta515@gmail.com>
2024-02-25 16:34:42 +0000

    restaurent-page

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import RestaurantPage from './components/RestaurantPage'
+import './App.css'
+
+// write your code here
+const App = () => <RestaurantPage />
+
+export default App
---
+++ src/components/RestaurantPage/index.js
@@ -0,0 +1,201 @@
+import {Component} from 'react'
+
+import {AiOutlineShoppingCart} from 'react-icons/ai'
+
+import './index.css'
+
+class RestaurantPage extends Component {
+  state = {activeTab: '11', categoryList: [], cartCount: 0}
+
+  componentDidMount() {
+    this.getData()
+  }
+
+  getData = async () => {
+    const url = 'https://run.mocky.io/v3/77a7e71b-804a-4fbd-822c-3e365d3482cc'
+    const response = await fetch(url)
+    const data = await response.json()
+
+    if (response.ok === true) {
+      const modifiedList = data[0].table_menu_list.map(each => ({
+        categoryDishes: each.category_dishes.map(emo => ({
+          addonCat: emo.addonCat,
+          dishAvailability: emo.dish_Availability,
+          dishType: emo.dish_Type,
+          dishCalories: emo.dish_calories,
+          dishCurrency: emo.dish_currency,
+          dishDescription: emo.dish_description,
+          dishId: emo.dish_id,
+          dishImage: emo.dish_image,
+          dishName: emo.dish_name,
+          dishPrice: emo.dish_price,
+          count: 0,
+        })),
+        menuCategory: each.menu_category,
+        menuCategoryId: each.menu_category_id,
+      }))
+
+      this.setState({categoryList: modifiedList})
+    }
+  }
+
+  render() {
+    const {activeTab, categoryList, cartCount} = this.state
+
+    const activeObj = categoryList.filter(
+      each => each.menuCategoryId === activeTab,
+    )
+
+    return (
+      <div className="restaurant-page">
+        <nav className="nav-bar">
+          <h1 className="main-heading">UNI Resto Cafe</h1>
+          <div className="cart-container-with-count">
+            <p className="my-orders">My Orders</p>
+            <AiOutlineShoppingCart size={30} />
+            <p className="cart-count-background">{cartCount}</p>
+          </div>
+        </nav>
+
+        {categoryList.length > 0 && (
+          <ul className="tabs_menu">
+            {categoryList.map(tab => (
+              <li key={tab.menuCategoryId}>
+                <button
+                  onClick={() => {
+                    this.setState({activeTab: tab.menuCategoryId})
+                  }}
+                  className={
+                    activeTab === tab.menuCategoryId ? 'tab extra' : 'tab'
+                  }
+                  type="button"
+                >
+                  {tab.menuCategory}
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
+        {activeObj.length > 0 && (
+          <ul className="dishes-container">
+            {activeObj[0].categoryDishes.map(each => (
+              <li className="dish-list-item" key={each.dishId}>
+                <div className="dish-item">
+                  {each.dishType === 2 ? (
+                    <div className="veg-container">
+                      <div className="veg-symbol">.</div>
+                    </div>
+                  ) : (
+                    <div className="non-veg-container">
+                      <div className="non-veg-symbol">.</div>
+                    </div>
+                  )}
+                  <div className="item-details">
+                    <h1 className="dish-name">{each.dishName}</h1>
+                    <p className="dish-price">
+                      {each.dishCurrency} {each.dishPrice}
+                    </p>
+                    <p className="dish-description">{each.dishDescription}</p>
+                    <div className="increment-decrement-buttons">
+                      <button
+                        onClick={() => {
+                          const newList = categoryList.map(newItem => {
+                            if (
+                              newItem.menuCategoryId ===
+                              activeObj[0].menuCategoryId
+                            ) {
+                              const updateItem = newItem.categoryDishes.map(
+                                tem => {
+                                  if (tem.dishId === each.dishId) {
+                                    if (tem.count > 0) {
+                                      return {...tem, count: tem.count - 1}
+                                    }
+                                    return tem
+                                  }
+                                  return tem
+                                },
+                              )
+                              return {
+                                ...newItem,
+                                categoryDishes: updateItem,
+                              }
+                            }
+                            return newItem
+                          })
+                          console.log(newList)
+
+                          this.setState({categoryList: newList})
+                          if (cartCount > 0) {
+                            this.setState(prevState => ({
+                              cartCount: prevState.cartCount - 1,
+                            }))
+                          }
+                        }}
+                        className="minus-button"
+                        type="button"
+                      >
+                        -
+                      </button>
+                      <p className="cart-count">{each.count}</p>
+                      <button
+                        onClick={() => {
+                          const newList = categoryList.map(newItem => {
+                            if (
+                              newItem.menuCategoryId ===
+                              activeObj[0].menuCategoryId
+                            ) {
+                              const updateItem = newItem.categoryDishes.map(
+                                tem => {
+                                  if (tem.dishId === each.dishId) {
+                                    return {...tem, count: tem.count + 1}
+                                  }
+                                  return tem
+                                },
+                              )
+                              return {
+                                ...newItem,
+                                categoryDishes: updateItem,
+                              }
+                            }
+                            return newItem
+                          })
+                          console.log(newList)
+                          this.setState({categoryList: newList})
+
+                          this.setState(prevState => ({
+                            cartCount: prevState.cartCount + 1,
+                          }))
+                        }}
+                        className="plus-button"
+                        type="button"
+                      >
+                        +
+                      </button>
+                    </div>
+                    {each.addonCat.length > 0 && (
+                      <p className="addon-cat">Customizations available</p>
+                    )}
+                    {each.dishAvailability === false && (
+                      <p className="dish-availability">Not available</p>
+                    )}
+                  </div>
+                  <div className="calories-container">
+                    <p className="calories">{each.dishCalories}</p>
+                    <p className="calories">calories</p>
+                  </div>
+                  <img
+                    className="dish-image"
+                    src={each.dishImage}
+                    alt={each.dishName}
+                  />
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    )
+  }
+}
+
+export default RestaurantPage
